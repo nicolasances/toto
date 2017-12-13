3b94e0f34c6cedfe5e41a39ddb84c8f0892d993b
nicolas <nicolas.matteazzi@gmail.com>
2017-12-13 20:36:01 +0100

    Draft drawing pad

--- toto/www/js/directives/totoDirectives.js
+++ toto/www/js/directives/totoDirectives.js
@@ -458,3 +458,72 @@ totoDirectivesModule.directive('totoBarGraph', function($rootScope, $timeout, $i
 		}
 	};
 });
+
+
+totoDirectivesModule.directive('totoDrawingPad', function($rootScope, $timeout, $interval) {
+	
+	return {
+		scope : {
+		},
+		templateUrl : 'directives/toto-drawing-pad.html',
+		link : function(scope) {
+			
+			context = document.getElementById('canvasInAPerfectWorld').getContext("2d");
+			
+			document.getElementById('canvasInAPerfectWorld').onmousedown = function(e){
+			  var mouseX = e.pageX - this.offsetLeft;
+			  var mouseY = e.pageY - this.offsetTop;
+					
+			  paint = true;
+			  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+			  redraw();
+			}
+			
+			document.getElementById('canvasInAPerfectWorld').onmousemove = function(e){
+			  if(paint){
+			    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+			    redraw();
+			  }
+			}
+			
+			document.getElementById('canvasInAPerfectWorld').onmouseup = function(e){
+				  paint = false;
+				}
+			
+			document.getElementById('canvasInAPerfectWorld').onmouseleave = function(e){
+				  paint = false;
+				}
+			
+			var clickX = new Array();
+			var clickY = new Array();
+			var clickDrag = new Array();
+			var paint;
+
+			function addClick(x, y, dragging) {
+			  clickX.push(x);
+			  clickY.push(y);
+			  clickDrag.push(dragging);
+			}
+			
+			function redraw(){
+				  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+				  
+				  context.strokeStyle = "#df4b26";
+				  context.lineJoin = "round";
+				  context.lineWidth = 5;
+							
+				  for(var i=0; i < clickX.length; i++) {		
+				    context.beginPath();
+				    if(clickDrag[i] && i){
+				      context.moveTo(clickX[i-1], clickY[i-1]);
+				     }else{
+				       context.moveTo(clickX[i]-1, clickY[i]);
+				     }
+				     context.lineTo(clickX[i], clickY[i]);
+				     context.closePath();
+				     context.stroke();
+				  }
+				}
+		}
+	}
+});
