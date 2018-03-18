9357e97507de2cb83eae97542ccf690d54a87ac1
nicolas <nicolas.matteazzi@gmail.com>
2018-03-18 18:08:22 +0100

    Abs & Obliques management

--- toto/www/js/config.js
+++ toto/www/js/config.js
@@ -1,7 +1,7 @@
 
 var microservicesProtocol = "http";
-var microservicesHost = "35.225.238.126";
+var microservicesHost = "35.193.14.80";
 var microservicesPort = "80";
-var microservicesUrl = "35.225.238.126";
-var microservicesUrl2 = "35.225.238.126";
+var microservicesUrl = "35.193.14.80";
+var microservicesUrl2 = "35.193.14.80";
 var imagesServerPort = "80";
